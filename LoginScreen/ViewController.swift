56dd02ca8a2d38da129105e1b60dc5a4edfa2dae
Javon Calloway <jtcis67@icloud.com>
2023-01-18 19:14:03 -0500

    Initial Commit

---
+++ LoginScreen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginScreen
+//
+//  Created by Javon Calloway on 1/18/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
